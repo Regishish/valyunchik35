c299e3cbc76f15a3265ae67cc7e2e64eb4f58062
Regishish <39839687+Regishish@users.noreply.github.com>
2025-06-03 22:53:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,14 +186,15 @@ await send_next_quest(user_id)
 
 user_states = {}
 quiz_progress = {}
-@dp.message_handler(commands=['start'])
-async def start_game(message: types.Message):
-if message.from_user.id in USER_IDS:
-    user_id = message.from_user.id
-    user_states[user_id] = 0
-    quiz_progress[user_id] = 0
-    await message.answer("🎉 Поздравляю, ты в игре! Сегодня тебя ждёт несколько заданий, а в конце — подарочек.")
-    await send_next_quest(user_id)
+@dp.message_handler()
+async def some_func(message: types.Message):
+    if message.from_user.id in USER_IDS:
+        await message.answer("✅ Принято!")
+        user_id = message.from_user.id
+        user_states[user_id] = 0
+        quiz_progress[user_id] = 0
+        await message.answer("🎯 Поздравляю, ты в игре! Сегодня тебя ждёт несколько заданий, а в конце — подарочек.")
+        await send_next_quest(user_id)
 
 async def send_next_quest(user_id):
 index = user_states.get(user_id, 0)
